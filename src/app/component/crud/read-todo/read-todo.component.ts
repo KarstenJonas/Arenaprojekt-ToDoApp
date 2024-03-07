85eaf1a93f51b4ef180437c2bce4c736cdbe943e
KarstenJonas <Karsten.Jonas@gmx.de>
2024-03-07 11:58:52 +0100

    [NEW] components created (A-2)
    components, services and class auto generated with ng generate component/service/class

---
+++ src/app/component/crud/read-todo/read-todo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-read-todo',
+  templateUrl: './read-todo.component.html',
+  styleUrl: './read-todo.component.scss'
+})
+export class ReadTodoComponent {
+
+}
